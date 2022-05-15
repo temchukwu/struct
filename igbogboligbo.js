0d395cc36d83a40f17beb42213ba06df9e5233c1
temchukwu <godblesschuks@yahoo.com>
2022-05-15 14:16:18 +0100

    velocity

---
+++ igbogboligbo.js
@@ -0,0 +1,15 @@
+let fruits_List = {
+    orange: 32,
+    mango: 13,
+    banana: 54,
+    apple: 86
+    }
+    console.log(fruits_List);
+    
+    let arr = [1,2,3,4,5,6,7];
+    console.log(arr.indexOf[`5`]);
+    
+    
+    
+    
+    
